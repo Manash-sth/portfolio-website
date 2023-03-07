d36f2abc020a9f303e0844c5b7c0a9d839bd9296
Manash-sth <manash.81142615@gmail.com>
2023-03-07 20:06:30 +0545

    website under construction

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>🚧 Website Under Construction 🚧</h1>
+<p>My <a href="https://github.com/Manash-sth" target="_blank" rel="noreferrer">GitHub</a></p>
+<p>My <a href="https://www.linkedin.com/in/manash-prajapati/" target="_blank" rel="noreferrer">LinkedIn</a></p>
