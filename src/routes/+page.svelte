c2ea611a75c519f3a2115a4ff7b2a97ae6fcbb34
Manash-sth <manash.81142615@gmail.com>
2023-03-08 13:22:37 +0545

    bg changes and other additions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,62 @@
 <script>
-
+    
 </script>
 
 <center>
-    <h1>🚧 Website Under Construction 🚧</h1>
-    <p>My <a href="https://github.com/Manash-sth" target="_blank" rel="noreferrer">GitHub</a></p>
-    <p>My <a href="https://www.linkedin.com/in/manash-prajapati/" target="_blank" rel="noreferrer">LinkedIn</a></p>
+    <h3 id="sub_txt">
+        Hey, You've reached Manash...
+    </h3>
+
+    <h1 id="main_text">
+        HELLO WORLD!
+    </h1>
+
+    <div id="connect_ico">
+        <p id="reach_me">
+            Reach me:   
+        </p>
+
+        <a href="mailto: manash.81142615@gmail.com" target="_blank" class="links">
+            <img src="%sveltekit.assets%/email.png" alt="eml">
+        </a>
+
+        <a href="https://www.linkedin.com/in/manash-prajapati/" target="_blank" rel="noreferrer" class="links">
+            <img src="%sveltekit.assets%/linkedin.png" alt="lin">
+        </a>
+
+        <a href="https://github.com/Manash-sth" target="_blank" rel="noreferrer" class="links">
+            <img src="%sveltekit.assets%/github.png" alt="git">
+        </a>
+    </div>
+
+
+
 </center>
 
 <style>
-    /* p {
-        
-    } */
+    h3, h1, p {
+        color: aliceblue;
+        font-family: 'sono';
+        font-weight: 300;
+    }
+
+    h1 {
+        font-size: 8rem;
+    }
+
+    #connect_ico {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        bottom: 0;
+        position: absolute;
+        margin-left: 1rem;
+        margin-bottom: 1rem;
+    }
+
+    .links {
+        margin-left: 1rem;
+    }
+    
 
 </style>
