53daa53cae49bf44ccd4d9abb9f805d884097b9f
Manash-sth <manash.81142615@gmail.com>
2023-03-08 11:13:56 +0545

    background and text center.. temp,,

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,16 @@
-<h1>🚧 Website Under Construction 🚧</h1>
-<p>My <a href="https://github.com/Manash-sth" target="_blank" rel="noreferrer">GitHub</a></p>
-<p>My <a href="https://www.linkedin.com/in/manash-prajapati/" target="_blank" rel="noreferrer">LinkedIn</a></p>
+<script>
+
+</script>
+
+<center>
+    <h1>🚧 Website Under Construction 🚧</h1>
+    <p>My <a href="https://github.com/Manash-sth" target="_blank" rel="noreferrer">GitHub</a></p>
+    <p>My <a href="https://www.linkedin.com/in/manash-prajapati/" target="_blank" rel="noreferrer">LinkedIn</a></p>
+</center>
+
+<style>
+    /* p {
+        
+    } */
+
+</style>
